131cc7385523b6b1df860048f68d7d536216bf9c
fover0932 <fover0932@gmail.com>
2015-08-12 22:39:17 +0800

    list all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,12 +6,19 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def index
+    @articles = Article.all
+  end
+
   def create
 	@article = Article.new(article_params)
 
 	@article.save
 	redirect_to @article	  	
   end
+
+
+  
   private
   def article_params
   	params.require(:article).permit(:title, :text)
