17da67557bb2fb12bc1d894b6d5a547a7e069c90
fover0932 <fover0932@gmail.com>
2015-08-30 00:46:29 +0800

    add update method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
 
@@ -21,7 +25,15 @@ class ArticlesController < ApplicationController
     end
   end
 
-
+  def update
+    @article = Article.find(params[:id])
+    
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'  
+    end
+  end
   
   private
   def article_params
